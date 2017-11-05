0428647a80929e3380700009a79b43b3a4d22044
aong <yxwzaxns@gmail.com>
2017-11-05 23:28:05 +0800

    add django vote code

---
+++ polls/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+import datetime
+from django.utils import timezone
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+        # return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
